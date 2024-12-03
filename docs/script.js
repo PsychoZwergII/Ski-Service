f5ca052045c9bcf33a63ef251dc430d69ae4c1d5
PsychoZwergII <leon.egli@student.ipso.ch>
2024-12-03 12:55:08 +0100

    name pattern

--- docs/script.js
+++ docs/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   function validateForm() {
-    const name = document.getElementById("name").value;
-    const namePattern = /^[a-zA-ZäöüÄÖÜß]+$/;
+    const name = document.getElementById("name").value.trim();
+    const namePattern = /^[a-zA-ZäöüÄÖÜß]+(?:\s[a-zA-ZäöüÄÖÜß]+)+$/;
     const email = document.getElementById("email").value;
     const phone = document.getElementById("phone").value;
     const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
